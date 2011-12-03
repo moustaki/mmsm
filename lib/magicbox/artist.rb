70aa841816d88ade8428a9a274c46f7fdcfb510e
Yves Raimond <yves.raimond@bbc.co.uk>
2011-12-03 14:34:54 +0000

    Seevl URI

--- lib/magicbox/artist.rb
+++ lib/magicbox/artist.rb
@@ -19,6 +19,16 @@ module Magicbox
        return data['response']['artist']['foreign_ids'][0]['foreign_id'].split(':').last
     end
 
+    def seevl_uri
+      data = JSON.parse(RestClient.get ENV['SEEVL_SPARQL'], { :params => {
+        :query  => "SELECT ?a WHERE { ?a <http://purl.org/ontology/mo/musicbrainz> <http://musicbrainz.org/artist/#{mbid}> }", 
+        :format => 'json'
+      }})
+      bindings = data['results']['bindings']
+      raise Exception.new 'No corresponding Seevl URI' if bindings.empty?
+      return bindings[0]['a']['value']
+    end
+
   end
 
 end
