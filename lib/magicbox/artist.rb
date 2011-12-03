9aa9e04264866395ee284777c3e29f8809dcf3a8
Yves Raimond <yves.raimond@bbc.co.uk>
2011-12-03 14:18:21 +0000

    Adding mbid for artist

--- lib/magicbox/artist.rb
+++ lib/magicbox/artist.rb
@@ -9,6 +9,16 @@ module Magicbox
       @id = id
     end
 
+    def mbid
+      data = JSON.parse(RestClient.get 'http://developer.echonest.com/api/v4/artist/profile', { :params => {
+          :api_key => ENV['ECHONEST_API_KEY'],
+          :format  => 'json',
+          :id      => @id,
+          :bucket  => 'id:musicbrainz'
+       }})
+       return data['response']['artist']['foreign_ids'][0]['foreign_id'].split(':').last
+    end
+
   end
 
 end
