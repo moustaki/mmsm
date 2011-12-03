af7304f3067f56420de4443100a236aa91bf8425
Yves Raimond <yves.raimond@bbc.co.uk>
2011-12-03 23:38:22 +0000

    Exp decay everywhere

--- lib/magicbox/track.rb
+++ lib/magicbox/track.rb
@@ -50,28 +50,34 @@ module Magicbox
 
     def self.avatar_colour(tracks)
       key = 0.0
-      tracks.each do |track|
-        key += track.key
+      norm = 0.0
+      tracks.each_with_index do |track, i|
+        key += track.key * (i ** @decay)
+        norm += (i ** @decay)
       end
-      key /= (tracks.size * 11.0)
+      key /= (norm * 11.0)
       return key
     end
 
     def self.avatar_frequency(tracks)
       tempo = 0.0
-      tracks.each do |track|
-        tempo += track.tempo
+      norm = 0.0
+      tracks.each_with_index do |track, i|
+        tempo += track.tempo * (i ** @decay)
+        norm += (i ** @decay)
       end
-      tempo /= (tracks.size * 60)
+      tempo /= (norm * 60)
       return tempo
     end
 
     def self.avatar_speed(tracks)
       amp = 0.0
-      tracks.each do |track|
-        amp += track.danceability
+      norm = 0.0
+      tracks.each_with_index do |track, i|
+        amp += track.danceability * (i ** @decay)
+        norm += i ** @decay
       end
-      amp /= tracks.size
+      amp /= norm
       return amp
     end
 
@@ -88,10 +94,12 @@ module Magicbox
 
     def self.avatar_amplitude(tracks)
       nrj = 0.0
-      tracks.each do |track|
-        nrj += track.energy
+      norm = 0.0
+      tracks.each_with_index do |track, i|
+        nrj += track.energy * (i ** @decay)
+        norm += i ** @decay
       end
-      nrj /= tracks.size
+      nrj /= norm
       return nrj
     end
 
