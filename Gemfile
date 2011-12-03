6c31fd3fd621309ea3c02ee982758cd4cfb65750
Patrick Sinclair <metade@gmail.com>
2011-12-03 13:27:51 +0000

    Adding mongodb

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source :rubygems
 gem 'sinatra'
 gem 'sinatra-twitter-oauth'
 
+gem 'mongoid'
+gem 'bson_ext'
+
 group :development do
   gem 'shotgun'
 end
--- app.rb
+++ app.rb
@@ -2,12 +2,16 @@ require 'yaml'
 require 'sinatra'
 require 'sinatra-twitter-oauth'
 
+require './models'
+
 configure do
+  Base.setup
   set :sessions, true
   @@config = YAML.load_file("config.yml") rescue nil || {}
-  set :twitter_oauth_config,  :key => ENV['CONSUMER_KEY'] || @@config['consumer_key'],
-                              :secret   => ENV['CONSUMER_SECRET'] || @@config['consumer_secret'],
-                              :callback => ENV['CALLBACK_URL'] || @@config['callback_url']
+  set :twitter_oauth_config,  
+    :key => ENV['CONSUMER_KEY'] || @@config['consumer_key'],
+    :secret   => ENV['CONSUMER_SECRET'] || @@config['consumer_secret'],
+    :callback => ENV['CALLBACK_URL'] || @@config['callback_url']
 end
 
 get '/' do
